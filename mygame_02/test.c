b6d7f11d00243173f8f35573dd8696e8074ad1fe
60853 <3442261673@qq.com>
2023-08-23 09:39:58 +0800

    1

--- mygame_02/test.c
+++ mygame_02/test.c
@@ -897,19 +897,12 @@ void test4()	//ÅÅĞòÊı×Ö
 	}
 }
 
-int main()
-{
-	test1();
-	test2();
-	test3();
-	test4();
-	return 0;
-}
-int main()
-{
-	test1();
-	test2();
-	test3();
-	test4();
-	return 0;
-}
+//int main()
+//{
+//	test1();
+//	test2();
+//	test3();
+//	test4();
+//	return 0;
+//}
+
